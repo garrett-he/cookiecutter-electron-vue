71a80254f815ef28e4f77c750e27ec1207b3b114
Garrett HE <garrett.he@hotmail.com>
2022-12-22 11:56:24 +0800

    Template: Create application with electron

--- {{cookiecutter.project_slug}}/vite.config.ts
+++ {{cookiecutter.project_slug}}/vite.config.ts
@@ -4,9 +4,22 @@ import vue from "@vitejs/plugin-vue2";
 {% else %}
 import vue from "@vitejs/plugin-vue";
 {% endif %}
+import electron from "vite-plugin-electron";
 
 export default {
     plugins: [
+        electron([
+            {
+                entry: ["./electron/index.ts", "./electron/preload.ts"],
+                vite: {
+                    resolve: {
+                        alias: {
+                            "@electron": path.resolve(__dirname, "electron")
+                        }
+                    }
+                }
+            }
+        ]),
         vue()
     ],
     resolve: {
